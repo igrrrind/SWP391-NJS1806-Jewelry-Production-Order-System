3b489434d3d8863f40d19e068e9e01fd9fa82dc7
igrrrind <winnguten@gmail.com>
2024-06-07 13:49:03 +0700

    revoke secret

--- frontend/fe-dao/src/services/Firebase.js
+++ frontend/fe-dao/src/services/Firebase.js
@@ -9,13 +9,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDZh5llwjiJR4uvAwkDgko1G7vFKx27MvM",
-  authDomain: "jewelry-production-order.firebaseapp.com",
-  projectId: "jewelry-production-order",
-  storageBucket: "jewelry-production-order.appspot.com",
-  messagingSenderId: "758144358433",
-  appId: "1:758144358433:web:ee5046e954e9015df7ab60",
-  measurementId: "G-B6TF05FTS4"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
