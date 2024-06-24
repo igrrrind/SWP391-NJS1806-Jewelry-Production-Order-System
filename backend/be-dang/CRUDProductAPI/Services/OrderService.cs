e36ebe8e30564a720527c50c011fdcfffeadb3af
xxDEEps <quangdang0704@gmail.com>
2024-06-24 14:16:24 +0700

    final dang

---
+++ backend/be-dang/CRUDProductAPI/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using Repositories;
+using Repositories.Dto;
+using Repositories.Models;
+using Repositories.QueryObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class OrderService
+    {
+        private OrderRepository _orderRepo = new OrderRepository();
+        public List<OrderDto> GetOrders(OrderQueryObject queryObject) => _orderRepo.GetOders(queryObject);
+       
+        public void AddNewOrder(Order order) => _orderRepo.AddNewOrder(order);
+    }
+}
---
+++ backend/be-dang/CRUDProductAPI/Services/ProductService.cs
@@ -0,0 +1,46 @@
+﻿using Repositories.Dto;
+using Repositories;
+using Repositories.Models;
+using Repositories.QueryObjects;
+
+namespace Services
+{
+    public class ProductService
+    {
+        ProductRepository _ProductRepo = new ProductRepository();
+        OriginalProductTypeRepository _originalProductTypeRepo = new OriginalProductTypeRepository();
+        StockService _stockService = new StockService();
+        public List<ProductDto> GetAllProduct(ProductQueryObject productQuery)
+        {
+            return _ProductRepo.GetAllProducts(productQuery);
+        }
+
+
+        public ProductDto? GetProductById(int id)
+        {
+            return _ProductRepo.GetProductById(id);
+        }
+        //CREATE
+        public void CreateProduct(Product product)
+        { 
+            _ProductRepo.CreateProduct(product);
+        }
+
+        //UPDATE
+        public void UpdateProduct(Product product) 
+        {
+            _ProductRepo.UpdateProduct(product);
+        }
+        //DELETE
+        public void DeleteProduct(int id)
+        {
+            _ProductRepo.DeleteProduct(id);
+        }
+
+        //GET ALL PRODUCT TYPE
+        public List<ProductType> GetAllProductTypes()
+        {
+            return _originalProductTypeRepo.GetAllProductTypes();
+        }
+    }
+}
