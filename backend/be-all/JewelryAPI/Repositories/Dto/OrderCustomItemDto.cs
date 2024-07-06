bcddec0752debb0bb23e868a0c059fd4ca0e8355
igrrrind <107808162+igrrrind@users.noreply.github.com>
2024-07-06 17:33:44 +0700

    update custom order item to return gemstoneColor

--- backend/be-all/JewelryAPI/Repositories/Dto/OrderCustomItemDto.cs
+++ backend/be-all/JewelryAPI/Repositories/Dto/OrderCustomItemDto.cs
@@ -12,7 +12,7 @@ public class OrderCustomItemDto
     public int ProductTypeId { get; set; }
     public int GemstoneId { get; set; }
     public string GemstoneType { get; set; }
-
+    public string GemstoneColor { get; set; }
     public int MetalId { get; set; }
     public string MetalTypeName { get; set; }
     public int? Size { get; set; }
